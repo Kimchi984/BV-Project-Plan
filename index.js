730af7e81fec4e2264efb3f1e23cc530a4424413
Unknown <alina.jaqueline@gmail.com>
2018-05-31 19:01:01 +0100

    add
    add for tasks and new milestone

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 $('.js-add-milestone').on('click', function(){
     $('.js-button-wrap').before($('#milestonefull').html())
 });
+
+
+
+$('form').on('click', '.js-add-task', function(event){
+    $(event.target).closest('.form-row').after($('#new-task-row').html())
+});
+
+var milestone = "Milestone"
+var task = "Task/Deliverable"
+
+console.log(`${milestone} ` + 1);
+
+$('.js-add-milestone').on('click', function(){
+    console.log(`${milestone} ` + 1);
+});
